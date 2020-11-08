a2ecf82716d5272df23d52e6b6d0a1437602d813
SanityPirate <brayden.waugh@gmail.com>
2020-11-08 16:00:34 -0500

    added opioid prescriptions by county heatmap

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# West Virginia Population data from:
+# https://www.census.gov/data/datasets/time-series/demo/popest/2010s-counties-total.html
+# Opioid Prescriptions by County data from: https://www.cdc.gov/drugoverdose/maps/rxcounty2017.html
+# Covid Case by County data from: https://github.com/nytimes/covid-19-data/blob/master/us-counties.csv
+# Covid Deaths by County data from:
+# https://data.cdc.gov/NCHS/Provisional-COVID-19-Death-Counts-in-the-United-St/kn79-hsxy
+
+
 import json
 import pandas as pd
 import plotly.express as px
@@ -12,6 +20,11 @@ popFile = 'wv-census.xlsx'
 popData = pd.read_excel(popFile)
 popDF = pd.DataFrame(popData, columns=['FIPS', 'Geographic Area', 'Census'])
 
+# Load opioid prescription dataset and convert to dataframe
+opioidFile = 'opioid-county.xlsx'
+opioidData = pd.read_excel(opioidFile)
+opioidDF = pd.DataFrame(opioidData, columns=['FIPS', 'County', 'Opioid Prescribing Rate per 100'])
+
 # Load covid deaths by county dataset and convert to dataframe
 covidFile = 'covid-county.csv'
 covidData = pd.read_csv(covidFile, usecols=
@@ -23,6 +36,25 @@ infectFile = 'covid-infect.csv'
 infectData = pd.read_csv(infectFile, usecols=
                          ['county', 'fips', 'cases'])
 infectDF = pd.DataFrame(infectData)
+
+# Assemble heatmap for opioid cases by county
+opioidFig = px.choropleth(opioidDF,
+                         geojson=counties,
+                         locations='FIPS',
+                         color='Opioid Prescribing Rate per 100',
+                         color_continuous_scale="OrRd",
+                         range_color=(0, 200),
+                         scope="usa",
+                         hover_name='County',
+                         hover_data={'FIPS': False,
+                                     'Opioid Prescribing Rate per 100': True},
+                         labels={'Opioid Prescribing Rate per 100': 'Opioid Prescribing Rate (per 100) '}
+                         )
+opioidFig.update_geos(fitbounds="locations", visible=False)  # Zoom map to WV
+opioidFig.update_layout(margin={"r": 100, "t": 100, "l": 100, "b": 100},
+                     title_text='2017 West Virginia County Opioid Prescribing Rates per 100 People')
+
+opioidFig.show()
 
 # Assemble heatmap for covid cases by county
 infectFig = px.choropleth(infectDF,
